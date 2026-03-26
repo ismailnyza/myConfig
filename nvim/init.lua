e836379962ac0535e2fb332bd3af4f714c426955
ismaelN <ismailnyzarsl@gmail.com>
2026-03-26 22:22:40 +0530

    Adjust visual indentation and tree width

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,8 +21,9 @@ vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.ignorecase = true
@@ -129,7 +130,7 @@ require("lazy").setup({
         },
         window = {
           position = "right",
-          width = 34,
+          width = 40,
         },
       })
     end,
